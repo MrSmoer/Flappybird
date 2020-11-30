65ff88a2ba48ac9925e10c72a0318ff505bec7a9
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2020-11-30 22:55:32 +0100

    asdf

--- Flappybird/main.java
+++ Flappybird/main.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-public class flappysworld extends World
+public class main extends World
 {
         HealthPoints hp0 = new HealthPoints(0);
         HealthPoints hp1 = new HealthPoints(1);
