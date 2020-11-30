43255a94e72923eda13256ab61db317141990899
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2020-11-30 18:03:20 +0100

    Health System Working

--- Flappybird/HealthPoints.java
+++ Flappybird/HealthPoints.java
@@ -11,6 +11,9 @@ public class HealthPoints extends Actor
     int id=0;
     int importantValue = 0;
     boolean active = false;
+    GreenfootImage full = new GreenfootImage("fullhp.png");
+    GreenfootImage half = new GreenfootImage("halfhp.png");
+    GreenfootImage dead = new GreenfootImage("nohp.png");
     public HealthPoints(int pId){
         id = pId;
 
@@ -21,42 +24,41 @@ public class HealthPoints extends Actor
 
     public void setImg(int health){
         //System.out.println("Meine id: " + id);
-        importantValue=health-(2*id);
+        importantValue=health+1-(2*id);
         //System.out.println("Important value "+importantValue);
         //System.out.println("my ID: "+importantValue+" healt: "+ health);
         switch(health){
             case 1:
-            case 2:
             if(id==0){
-                active=true;
-            }else active=false;
+                active = true;
+            }else active = false;
             break;
-
+            
+            case 2:
             case 3:
-            case 4:
             if(id==1){
-                active=true;
-            }else active=false;
+                active = true;
+            }else active = false;
             break;
 
+            case 4:
             case 5:
             case 6:
             if(id==2){
-                active=true;
-            }else active=false;
+                active = true;
+            }else active = false;
             break;
 
         }
         if(active){
-            System.out.println(" ");
+            //System.out.println(" ");
             if(importantValue==1){
-                System.out.println(id+" Ich zeige: 1");
+                //System.out.println(id+" Ich zeige: 1");
+                setImage(dead);
             }
             if(importantValue==2){
-                System.out.println(id+" Ich zeige: 2");
-            }
-            if(importantValue==3){
-                System.out.println(id+" Ich zeige: 3");
+                //System.out.println(id+" Ich zeige: 2");
+                setImage(half);
             }
             System.out.println(" ");
         }else ;//System.out.println(id+"zeigt"+importantValue);
